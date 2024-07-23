48e149274137e05de37ae83850370e8cab5578f5
kocyk908 <lukkoc01@gmail.com>
2024-07-23 17:57:00 +0200

    env

--- MS-Piotrek/src/env.c
+++ MS-Piotrek/src/env.c
@@ -1,51 +1,61 @@
 #include "minishell.h"
 
-char **global_envp;
+char	**g_envp;
 
-void init_global_envp(char **envp)
+void	init_global_envp(char **envp)
 {
-    int i;
-    for (i = 0; envp[i] != NULL; i++);
-    global_envp = malloc((i + 1) * sizeof(char *));
-    for (int j = 0; j < i; j++)
-    {
-        global_envp[j] = strdup(envp[j]);
-    }
-    global_envp[i] = NULL;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (envp[i] != NULL)
+		i++;
+	g_envp = malloc((i + 1) * sizeof(char *));
+	while (j < i)
+	{
+		g_envp[j] = strdup(envp[j]);
+		j++;
+	}
+	g_envp[i] = NULL;
 }
 
-void ft_env(char **envp)
+void	ft_env(char **envp)
 {
-    pid_t pid;
-    char *args[2];
-    int status;
+	pid_t	pid;
+	char	*args[2];
+	int		status;
 
-    args[0] = "env";
-    args[1] = NULL;
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        return;
-    }
-    if (pid == 0)
-    {
-        if (execve("/usr/bin/env", args, envp) == -1)
-        {
-            perror("execve");
-            exit(1);
-        }
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-    }
+	args[0] = "env";
+	args[1] = NULL;
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return ;
+	}
+	if (pid == 0)
+	{
+		if (execve("/usr/bin/env", args, envp) == -1)
+		{
+			perror("execve");
+			exit(1);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
 }
 
-void builtin_env(void)
+void	builtin_env(void)
 {
-    for (int i = 0; global_envp[i] != NULL; i++)
-    {
-        printf("%s\n", global_envp[i]);
-    }
+	int	i;
+
+	i = 0;
+	while (g_envp[i] != NULL)
+	{
+		printf("%s\n", g_envp[i]);
+		i++;
+	}
 }
