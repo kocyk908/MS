b5e622c0452a622b7a60d90fda4ceb056246105b
kocyk908 <lukkoc01@gmail.com>
2024-09-07 14:24:12 +0200

    case z  echo hi

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 14:02:12 by pruszkie          #+#    #+#             */
-/*   Updated: 2024/09/07 00:44:17 by marvin           ###   ########.fr       */
+/*   Updated: 2024/09/07 14:21:38 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,19 @@ char	*ft_dollar_check_vol2(t_gen *gen, char *token)
 			return (gen->envs[i] + env_len + 1);
 		i++;
 	}
+	return ("");
+}
+
+static void	ft_shift_arguments_left(char *env, t_arg *args, int start_index)
+{
+	int	j;
+
+	j = start_index;
+	while (args[j].arg)
+	{
+		args[j].arg = args[j + 1].arg;
+		j++;
+	}
 }
 
 void	ft_dollar_check(t_gen *gen, t_command *head)
@@ -68,9 +81,11 @@ void	ft_dollar_check(t_gen *gen, t_command *head)
 		if (ft_strcmp(head->args[i].arg, "echo") == 0
 			|| head->args[i].which_quotes == '\'')
 		{
-			i = i + 2;
+			i += 2;
 			continue ;
 		}
+		if (ft_strcmp(head->args[i].arg, "$") == 0)
+			gen->exit_status = 127;
 		if (head->args[i].arg[0] == '$')
 		{
 			if (head->args[i].arg[1] == '?')
@@ -79,7 +94,10 @@ void	ft_dollar_check(t_gen *gen, t_command *head)
 				env = ft_strdup(getenv("PWD"));
 			else
 				env = ft_dollar_check_vol2(gen, head->args[i].arg + 1);
-			head->args[i].arg = env;
+			if (*env == '\0')
+				ft_shift_arguments_left(env, head->args, i--);
+			else
+				head->args[i].arg = env;
 		}
 		i++;
 	}
